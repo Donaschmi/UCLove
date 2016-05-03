4bd1884303d0c9bb78708a3a4da4e4f20600ea1f
Robin-gielen <robin.gielen@student.uclouvain.be>
2016-05-03 21:08:29 +0200

    test up

--- app/src/main/java/ucloveproject/uclove/Activity/Friends.java
+++ app/src/main/java/ucloveproject/uclove/Activity/Friends.java
@@ -1,14 +1,22 @@
 package ucloveproject.uclove.Activity;
 
+import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.util.Log;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.RadioButton;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 import ucloveproject.uclove.DB.DatabaseHandler;
 import ucloveproject.uclove.DB.User;
@@ -22,6 +30,7 @@ public class Friends extends MyActivity implements View.OnClickListener {
 
     private CheckBox btnFav = null;
     private String username = null;
+    private ListView listview = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,6 +42,59 @@ public class Friends extends MyActivity implements View.OnClickListener {
             username = extras.getString("username");
 
         btnFav = (CheckBox) findViewById(R.id.show_fav);
+        listview = (ListView) findViewById(R.id.list_friends);
+        String[] friends_names; //= blablabla
+        final ArrayList<String> list = new ArrayList<String>();
+        for (int i = 0; i < friends_names.length; i++) {
+            list.add(friends_names[i]);
+        }
+        final StableArrayAdapter adapter = new StableArrayAdapter(this,
+                android.R.layout.simple_list_item_1, list);
+        listview.setAdapter(adapter);
 
+        listview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> parent, final View view,
+                                    int position, long id) {
+                final String item = (String) parent.getItemAtPosition(position);
+                view.animate().setDuration(2000).alpha(0)
+                        .withEndAction(new Runnable() {
+                            @Override
+                            public void run() {
+                                list.remove(item);
+                                adapter.notifyDataSetChanged();
+                                view.setAlpha(1);
+                            }
+                        });
+            }
+
+        });
+    }
+
+        private class StableArrayAdapter extends ArrayAdapter<String> {
+
+            HashMap<String, Integer> mIdMap = new HashMap<String, Integer>();
+
+            public StableArrayAdapter(Context context, int textViewResourceId,
+                                      List<String> objects) {
+                super(context, textViewResourceId, objects);
+                for (int i = 0; i < objects.size(); ++i) {
+                    mIdMap.put(objects.get(i), i);
+                }
+            }
+
+            @Override
+            public long getItemId(int position) {
+                String item = getItem(position);
+                return mIdMap.get(item);
+            }
+
+            @Override
+            public boolean hasStableIds() {
+                return true;
+            }
+
+        }
     }
 }
