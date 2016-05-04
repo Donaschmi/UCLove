9bb25dabf1e6d0ea35b8ffd8481065cb01b64d4a
Robin-gielen <robin.gielen@student.uclouvain.be>
2016-05-04 13:53:56 +0200

    Friendlist fonctionelle (layout)

--- app/src/main/java/ucloveproject/uclove/Activity/UserProfil.java
+++ app/src/main/java/ucloveproject/uclove/Activity/UserProfil.java
@@ -10,9 +10,9 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
-import com.google.android.gms.appindexing.Action;
-import com.google.android.gms.appindexing.AppIndex;
-import com.google.android.gms.common.api.GoogleApiClient;
+//import com.google.android.gms.appindexing.Action;
+//import com.google.android.gms.appindexing.AppIndex;
+//import com.google.android.gms.common.api.GoogleApiClient;
 
 import ucloveproject.uclove.DB.DatabaseHandler;
 import ucloveproject.uclove.DB.User;
